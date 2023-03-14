3152ed6ed4a8bc9d9b3e70927afdd003195a0ea5
Gideon <owolabigidslab@gmail.com>
2023-03-14 23:31:28 +0100

    designing sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
-import "./App.css";
 import SideBar from "./components/SideBar/SideBar";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
+    <div>
       <SideBar />
     </div>
   );
